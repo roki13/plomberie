74ea625377ba4512ef891d0c0e229db137761696
Sébastien Bicaïs <sebastien.bicais@rolex.com>
2019-07-08 21:32:10 +0200

    Add Material UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './assets/logo-cyril.jpg';
 import './App.css';
 import Title from './components/Title/Title';
 import Address from './components/Address/Address';
+import AppBar from './components/AppBar/AppBar'
 
 function App() {
   return (
@@ -10,16 +11,18 @@ function App() {
     
     <div className="App">
   
+    <AppBar></AppBar>
       <header className="App-header">
       
         <Title name="Bicaïs Plomberie" />
 
         <img src={logo} className="App-logo" alt="logo" with="350" />
+
         <Address />
+
       </header>
 
-      
-
+     
     </div>
   );
 }
